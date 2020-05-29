769f929d1c2cdb902c62102fb3ddf86ebc5ea09d
Noah Ziltener <noahziltener@yahoo.com>
2020-05-29 14:13:30 +0200

    added Prototyps

---
+++ Prototyp/Prototyp_Nick/Prototyp_Nick/ViewModel/HelloWorldViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Prototyp_Nick.Model;
+
+namespace Prototyp_Nick.ViewModel
+{
+    public class HelloWorldViewModel : INotifyPropertyChanged
+    {
+        private string helloString;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string HelloString
+        {
+            get
+            {
+                return helloString;
+            }
+            set
+            {
+                helloString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public HelloWorldViewModel()
+        {
+            HelloWorldModel helloWorldModel = new HelloWorldModel();
+            helloString = helloWorldModel.ImportantInfo;
+        }
+    }
+}
