897e8de17268d5f82f68257cbcb5556ecb2f8738
Nick <nick7@hotmail.ch>
2020-06-12 14:49:42 +0200

    edited Task Model

--- N-N_Scrumboard/N-N_Scrumboard/Model/Task.cs
+++ N-N_Scrumboard/N-N_Scrumboard/Model/Task.cs
@@ -7,6 +7,32 @@ using System.Threading.Tasks;
 namespace N_N_Scrumboard.Model
 {
     class Task
-    {
+    { 
+        private string _title;
+
+        public string Title
+        {
+            get { return _title; }
+
+            set { _title = value; }
+        }
+
+        private string _description;
+
+        public string Description
+        {
+            get { return _description; }
+
+            set { _description = value; }
+        }
+
+        /*private user _assinedTo;
+
+        public string AssinedTo
+        {
+            get { return _assinedTo; }
+
+            set { _assinedTo = value; }
+        }*/
     }
 }
